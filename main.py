19da3c5d5f159d95cc24d9e3150352a49cfb0c11
lewism20-ecu <lewism20@students.ecu.edu>
2023-11-07 12:32:16 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,291 @@
+# TODO: Make it so the user has to do the two factor to be logged in
+
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import psycopg2, os, random
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+from datetime import timedelta, datetime
+from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user
+
+# Set up for email
+load_dotenv()
+email_username = os.environ.get('EMAIL_USERNAME')
+email_password = os.environ.get('EMAIL_PASSWORD_CODE')
+
+# Set up for database
+conn = psycopg2.connect(
+    database='shield_pass',
+    user='raywu1990',
+    password='test',
+    host='127.0.0.1',
+    port='5432')
+cursor = conn.cursor()
+
+# Set up for user authentication
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+# Set up for flask server
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = email_username
+app.config['MAIL_PASSWORD'] = email_password
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+# Set the session duration to 30 days
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
+mail = Mail(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+app.secret_key = os.environ.get('SECRET_KEY')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if '_user_id' in session:
+        return redirect(url_for('login_homepage'))
+    if request.method == 'POST':
+        db_email = request.form['email_input']
+        db_password = request.form['password_input']
+        
+        cursor.execute("SELECT * FROM users WHERE email = %s AND password = %s;", (db_email, db_password))
+        result = cursor.fetchone()
+        
+        if result:
+            user_id = result[0]
+            user = User(user_id)
+            login_user(user)
+            random_code = ''.join(str(random.randint(0, 9)) for _ in range(5))
+            email_code = " ".join(random_code) 
+            session['verification_code'] = random_code
+            msg = Message('Shield Pass Two-Factor', sender='shield.pass.two.factor@gmail.com', recipients=[db_email])
+            # msg.body = f'Your sign-in code: {random_code}'
+            # msg.body = "Authenticate Your Account To make sure you’re account is secure, you’ll need to verify your identity. Please enter the code below to access your account 1 2 3 4 5 Thank you for using ShieldPass"
+            msg.html = render_template('emails/email_two_factor.html', email_code=email_code)
+            msg.content_subtype = 'html'
+            mail.send(msg)
+            return redirect(url_for('two_factor'))
+        else:
+            message = "Invalid username or password. Please try again."
+            return render_template('log_in.html', message=message)
+            
+    return render_template('log_in.html')
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    if '_user_id' in session:
+        return redirect(url_for('homepage'))
+    if request.method == 'POST':
+        email = request.form['email_input']
+        password = request.form['password_input']
+        
+        cursor.execute("SELECT * FROM users WHERE email=%s;", (email,))
+        user = cursor.fetchone()
+        
+        if user is None:
+            cursor.execute('INSERT INTO users (email, password) VALUES (%s, %s);', (email, password))
+            conn.commit()
+            cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s;', (email, password))
+            result = cursor.fetchone()
+            user_id = result[0]
+            user = User(user_id)
+            login_user(user)
+            return redirect(url_for('two_factor'))
+        else:
+            message = "Email already in use"
+            
+        return render_template('sign_up.html', message=message)
+        
+    return render_template('sign_up.html')
+
+@app.route('/two_factor', methods=['GET', 'POST'])
+@login_required
+def two_factor():
+    if request.method == 'POST':
+        input0 = request.form['input0']
+        input1 = request.form['input1']
+        input2 = request.form['input2']
+        input3 = request.form['input3']
+        input4 = request.form['input4']
+        stored_code = session.get('verification_code')
+        if (input0 == stored_code[0] and input1 == stored_code[1] and input2 == stored_code[2] and input3 == stored_code[3] and input4 == stored_code[4]):
+            return redirect(url_for('login_homepage'))
+        else:
+            message = "Code does not match"
+        
+        return render_template('enter_two_factor.html', message=message)
+    
+    return render_template('enter_two_factor.html')
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    if request.method == 'POST':
+        email = request.form['email_input']
+        
+        cursor.execute("SELECT * FROM users WHERE email=%s;", (email,))
+        user = cursor.fetchone()
+        
+        if user is not None:
+            msg = Message('Shield Pass Reset Password', sender='shield.pass.two.factor@gmail.com', recipients=[email])
+            # msg.body = f'Reset your password here: http://127.0.0.1:5000/new_password?email={email}'
+            email_msg = f'http://127.0.0.1:5000/new_password?email={email}'
+            msg.html = render_template('emails/email_send_email.html', email_link=email_msg)
+            msg.content_subtype = 'html'
+            mail.send(msg)
+            
+            return redirect(url_for('sent_password'))
+        else:
+            message = "Email not in use"
+        
+        return render_template('reset_password.html', message=message)
+    
+    return render_template('reset_password.html')
+
+@app.route('/new_password', methods=['GET', 'POST'])
+def new_password():
+    email = request.args.get('email')
+    
+    if request.method == 'POST':
+        password = request.form['password_input']
+        email = request.form['email']
+        
+        cursor.execute('UPDATE users SET password = %s WHERE email = %s;', (password, email))
+        conn.commit()
+        
+        return redirect(url_for('index'))
+        
+    return render_template('new_password.html', email=email)
+
+@app.route('/sent_password')
+def sent_password():
+    return render_template('sent_password.html')
+
+@app.route('/login_homepage', methods=['GET', 'POST'])
+@login_required
+def login_homepage():
+    user_id = session.get('_user_id')
+    if request.method == 'POST':
+        website = request.form['new-website']
+        email = request.form['new-email']
+        username = request.form['new-username']
+        password = request.form['new-password']
+        
+        cursor.execute('INSERT INTO login (uid, website, email, username, password) VALUES (%s, %s, %s, %s, %s);', (user_id, website, email, username, password))
+        conn.commit()
+        return redirect(url_for('login_homepage'))
+    
+    if request.args.get('buttonName') == 'Popular':
+        cursor.execute("SELECT lid, company FROM login WHERE uid = %s ORDER BY num_of_uses DESC;", (user_id,))
+        record = cursor.fetchall()
+        sql_table = [(item[0], item[1]) for item in record]
+        return jsonify(sql_table)
+    elif request.args.get('buttonName') == 'A-Z':
+        cursor.execute("SELECT lid, company FROM login WHERE uid = %s ORDER BY company;", (user_id,))
+        record = cursor.fetchall()
+        sql_table = [(item[0], item[1]) for item in record]
+        return jsonify(sql_table)
+    elif request.args.get('buttonName') == 'Z-A':
+        cursor.execute("SELECT lid, company FROM login WHERE uid = %s ORDER BY company DESC;", (user_id,))
+        record = cursor.fetchall()
+        sql_table = [(item[0], item[1]) for item in record]
+        return jsonify(sql_table)
+    elif request.args.get('buttonName') == 'Oldest':
+        cursor.execute("SELECT lid, company FROM login WHERE uid = %s ORDER BY updated_at;", (user_id,))
+        record = cursor.fetchall()
+        sql_table = [(item[0], item[1]) for item in record]
+        return jsonify(sql_table)
+    elif request.args.get('buttonName') == 'Newest':
+        cursor.execute("SELECT lid, company FROM login WHERE uid = %s ORDER BY updated_at DESC;", (user_id,))
+        record = cursor.fetchall()
+        sql_table = [(item[0], item[1]) for item in record]
+        return jsonify(sql_table)
+    else:
+        cursor.execute("SELECT lid, company FROM login WHERE uid = %s ORDER BY company;", (user_id,))
+        record = cursor.fetchall()
+        ids = [records[0] for records in record]
+        sql_table = [(item[1],) for item in record]
+        return render_template('login_homepage.html', sql_table=zip(ids, sql_table))
+
+@app.route('/update_row/<int:row_id>', methods=['PUT'])
+def update_row(row_id):
+    if request.method == 'PUT':
+        try:
+            cursor.execute("UPDATE login SET num_of_uses = num_of_uses + 1 WHERE lid = %s", (row_id,))
+            conn.commit()
+            return jsonify({'message': 'Row updated successfully.'})
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+
+@app.route('/get_login_info/<int:login_id>', methods=['GET'])
+def get_login_info(login_id):
+    cursor.execute("SELECT website, email, username, password FROM login WHERE lid = %s;", (login_id,))
+    record = cursor.fetchone()
+    if record:
+        return jsonify({'website': record[0], 'email': record[1], 'username': record[2], 'password': record[3]})
+    else:
+        return jsonify({'error': 'Login information not found'})
+    
+@app.route('/save_changes', methods=['POST'])
+def save_changes():
+    if request.method == 'POST':
+        data = request.get_json()
+        
+        # Extract the data from the JSON request
+        lid = data['lid']
+        website = data['website']
+        email = data['email']
+        username = data['username']
+        password = data['password']
+        
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # Update the data in the PostgreSQL database
+        cursor.execute("UPDATE login SET email = %s, username = %s, password = %s, website = %s, updated_at = %s WHERE lid = %s", (email, username, password, website, current_time, lid))
+        conn.commit()
+
+        return jsonify({'message': 'Data updated in the database.'})
+    
+@app.route('/delete_row', methods=['POST'])
+def delete_row():
+    data = request.get_json()
+    if data and 'row_id' in data:
+        row_id = data['row_id']
+        cursor.execute("DELETE FROM login WHERE lid = %s", (row_id,))
+        conn.commit()
+        return jsonify({'message': 'Row deleted successfully.'})
+    else:
+        return jsonify({'message': 'Invalid data format.'}), 400
+    
+@app.route('/search', methods=['POST'])
+def search():
+    data = request.get_json()
+    query = data['query']
+
+    cursor.execute("SELECT lid, company FROM login WHERE company ILIKE %s ORDER BY company;", ('%' + query + '%',))
+    record = cursor.fetchall()
+    sql_table = [(item[0], item[1]) for item in record]
+
+    return jsonify(sql_table)
+
+@app.route('/homepage')
+@login_required
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.debug = True
+    ip = '127.0.0.1'
+    # For https: ssl_context='adhoc'
+    app.run(host=ip)
---
+++ static/js/login_homepage.js
@@ -0,0 +1,343 @@
+var buttonClicked = false;
+let currentRowId;
+let currentSelectedButtonName = 'A-Z';
+
+function showButtons() {
+    var slideOutButtons = document.getElementById("filter-slide-out-buttons");
+    if (!buttonClicked) {
+        slideOutButtons.style.display = "block";
+        buttonClicked = true;
+    } else {
+        slideOutButtons.style.display = "none";
+        buttonClicked = false;
+    }
+}
+function changeColor(buttonName) {
+    currentSelectedButtonName = buttonName;
+    var buttons = document.getElementsByClassName('filter-show-buttons');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].style.color = 'white'; // Change all text colors to white
+    }
+    var selectedButton = document.querySelector('button[onclick="changeColor(\'' + buttonName + '\')"]');
+    selectedButton.style.color = '#0DBC62'; // Change the text color of the clicked button to green
+    
+    // Implement specific actions for each button here
+    if (buttonName === 'A-Z') {
+        fetch('/login_homepage?buttonName=A-Z', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+            updateTable(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        console.log('A-Z button clicked');
+    } else if (buttonName === 'Z-A') {
+        fetch('/login_homepage?buttonName=Z-A', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+            updateTable(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        console.log('Z-A button clicked');
+    } else if (buttonName === 'Oldest') {
+        fetch('/login_homepage?buttonName=Oldest', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+            updateTable(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        console.log('Oldest button clicked');
+    } else if (buttonName === 'Newest') {
+        fetch('/login_homepage?buttonName=Newest', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+            updateTable(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        console.log('Newest button clicked');
+    } else if (buttonName === 'Popular') {
+        fetch('/login_homepage?buttonName=Popular', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+            updateTable(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+}
+function updateTable(data) {
+    var table = document.querySelector('table');
+    
+    table.innerHTML = '';
+    
+    // Add new rows based on the updated data
+    for (var i = 0; i < data.length; i++) {
+        var row = table.insertRow(-1); // Insert at the end of the table
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = '<img class="logo-img" src="https://logo.clearbit.com/' + data[i][1] + '.com?size=45" alt="No Logo">';
+        cell1.classList.add('left-column');
+        cell2.innerHTML = data[i][1];
+        cell2.classList.add('right-column');
+        row.setAttribute('onclick', 'fetchLoginInfo(' + data[i][0] + ')');
+    }
+}
+function openConfirmModal() {
+    event.preventDefault();
+    document.getElementById("modal-confirm").style.display = "block";
+}
+function openViewModal() {
+    document.getElementById("myModal_view").style.display = "block";
+}
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+function closeModalConform() {
+    event.preventDefault();
+    document.getElementById("modal-confirm").style.display = "none";
+}
+function closeModalView() {
+    document.getElementById("myModal_view").style.display = "none";
+    document.getElementById("new-website-view").value = "";
+    document.getElementById("new-email-view").value = "";
+    document.getElementById("new-username-view").value = "";
+    document.getElementById("new-password-view").value = "";
+    document.querySelector('.save-button').style.display = 'none';
+    document.querySelector('.edit-button').style.display = 'inline-block';
+    document.querySelector('.delete-button').style.display = 'none';
+}
+function closeModal() {
+    var email_i = document.getElementById('invalid_email');
+    var inputs_i = document.getElementById('invalid_inputs');
+    inputs_i.style.display = 'none';
+    email_i.style.display = 'none';
+    document.getElementById("myModal").style.display = "none";
+    document.getElementById("new-website").value = "";
+    document.getElementById("new-email").value = "";
+    document.getElementById("new-username").value = "";
+    document.getElementById("new-password").value = "";
+}
+
+function ValidateEmail() {
+    event.preventDefault();
+    var validRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if ((document.getElementById('new-email')).value.match(validRegex)) {
+        showEmailMessage(true);
+        return true;
+    
+    } else {
+        showEmailMessage(false);
+        return false;
+    }
+    }
+
+    function showEmailMessage(input) {
+    var email_i = document.getElementById('invalid_email');
+    var inputs_i = document.getElementById('invalid_inputs');
+    
+    inputs_i.style.display = 'none';
+    email_i.style.display = 'none';
+    
+    if (input === true) {
+        checkInputs();
+    }
+    else {
+        email_i.style.display = 'block';
+    }
+    }
+
+    function checkInputs() {
+    var inputs_i = document.getElementById('invalid_inputs');
+    var input1 = document.getElementById("new-website").value;
+    var input2 = document.getElementById("new-email").value;
+    var input3 = document.getElementById("new-username").value;
+    var input4 = document.getElementById("new-password").value;
+    
+    if (input1 && input2 && input3 && input4) {
+        document.getElementById("loginForm").submit();
+    } else {
+        inputs_i.style.display = 'block';
+    }
+    }
+
+    function fetchLoginInfo(row_id) {
+    currentRowId = row_id;
+
+    // Perform an update call to increment a column by one
+    fetch(`/update_row/${row_id}`, {
+        method: 'PUT'
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Continue with fetching the login info if the update is successful
+        fetch(`/get_login_info/${row_id}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    alert(data.error);
+                } else {
+                    // Display the fetched data in the modal
+                    document.getElementById("new-website-view").value = data.website;
+                    document.getElementById("new-email-view").value = data.email;
+                    document.getElementById("new-username-view").value = data.username;
+                    document.getElementById("new-password-view").value = data.password;
+                    openViewModal();
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}        
+function copyText(inputId) {
+    event.preventDefault();
+    var inputElement = document.getElementById(inputId);
+    inputElement.select();
+    inputElement.setSelectionRange(0, 99999); // For mobile devices 
+    document.execCommand("copy");
+    
+    var originalHtml = document.getElementById(inputId).nextElementSibling.innerHTML;
+    document.getElementById(inputId).nextElementSibling.innerHTML = '<i class="fas fa-clipboard-check" style="color: #4CAF50; font-size: 30px;"></i>'; // Change to clipboard with check
+
+    setTimeout(function() {
+        document.getElementById(inputId).nextElementSibling.innerHTML = originalHtml; // Revert back to clone
+    }, 1000); // Revert after 1 second(s)
+}
+function makeEditable() {
+    event.preventDefault();
+
+    var copyButtons = document.querySelectorAll('.copy-button');
+    copyButtons.forEach(function(button) {
+        button.style.display = 'none';
+    });
+
+    // Make all input fields editable
+    var inputFields = document.querySelectorAll('.new-password-input');
+    inputFields.forEach(function(input) {
+        input.readOnly = false;
+    });
+
+    // Hide the edit button and display the save button
+    document.querySelector('.edit-button').style.display = 'none';
+    document.querySelector('.save-button').style.display = 'inline-block';
+    // Display the delete button
+    document.querySelector('.delete-button').style.display = 'inline-block';
+}
+function saveChanges() {
+    event.preventDefault();
+
+    let lid = currentRowId;
+    var website = document.getElementById('new-website-view').value;
+    var email = document.getElementById('new-email-view').value;
+    var username = document.getElementById('new-username-view').value;
+    var password = document.getElementById('new-password-view').value;
+
+    // Prepare the data payload to be sent to the Flask backend
+    var data = {
+        lid: lid,
+        website: website,
+        email: email,
+        username: username,
+        password: password
+    };
+
+    // Send the data to the Flask backend
+    fetch('/save_changes', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Data saved:', data);
+        showCloneIcons();
+        makeReadOnly();
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+    document.querySelector('.save-button').style.display = 'none';
+    document.querySelector('.edit-button').style.display = 'inline-block';
+
+    document.querySelector('.delete-button').style.display = 'none';
+}
+function showCloneIcons() {
+    var copyButtons = document.querySelectorAll('.copy-button');
+    copyButtons.forEach(function(button) {
+        button.style.display = 'inline-block';
+    });
+}
+function makeReadOnly() {
+    var inputFields = document.querySelectorAll('.new-password-input');
+    inputFields.forEach(function(input) {
+        input.readOnly = true;
+    });
+}
+function deleteEntry() {
+    event.preventDefault();
+
+    var data = {
+        row_id: currentRowId
+    };
+    fetch('/delete_row', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        closeModalView();
+        changeColor(currentSelectedButtonName);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+function searchDatabase() {
+    var inputText = document.getElementById('searchInput').value;
+    fetch('/search', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query: inputText })
+    })
+    .then(response => response.json())
+    .then(data => {
+        updateTable(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+function replaceWithErrorImage(image) {
+    image.onerror = null; // to avoid infinite loops in case the replacement image is also missing
+    image.src = "https://placehold.co/45/EEE/31343C?font=raleway&text=No-Image-Found"; // Replace with the URL of your placeholder image
+}
